1581454a5e3929da279ae7f5d05f3dd2a84d1566
aswinashok62 <143902263+aswinashok62@users.noreply.github.com>
2025-04-10 20:18:32 +0530

    settings.py

--- hospital/settings.py
+++ hospital/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-og%_8wo!-i9xp939qe)8m@wto+f(u2$3-t2ybt$dbwc0$d*my*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 AUTH_USER_MODEL = 'users.Users'
 
 
